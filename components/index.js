d420249ff0bae7a811303eb06ae118ed045dd22b
Terogene Claudio <cterogene@gmail.com>
2022-11-11 15:45:56 -0500

    updated the nft card components with style and images for better UX

--- components/index.js
+++ components/index.js
@@ -1,7 +1,7 @@
 import HomeHeader from "./HomeHeader";
 import FocusedStatusBar from "./FocusedStatusBar";
 import NFTCard from "./NFTCard";
-
+import {CircleButton, RectButton} from "./Button";
 export{
-    HomeHeader, FocusedStatusBar, NFTCard
+    HomeHeader, FocusedStatusBar, NFTCard, CircleButton, RectButton
 } 
