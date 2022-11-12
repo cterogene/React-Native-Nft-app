8310a0fd671d2d7c9aca171e1ec89c623af1fe17
Terogene Claudio <cterogene@gmail.com>
2022-11-11 19:38:42 -0500

    nft touchable button styled with heart design

--- components/Button.js
+++ components/Button.js
@@ -1,11 +1,29 @@
-import { View, Text } from 'react-native'
+import { TouchableOpacity, View, Text, Image } from 'react-native'
 import React from 'react'
+import {COLORS, SIZES, SHADOWS, assets} from '../constants';
+import { useLinkProps } from '@react-navigation/native';
 
-export const CircleButton = () => {
+export const CircleButton = ({imgUrl, handlePress, ...props}) => {
   return (
-    <View>
-      <Text>Button</Text>
-    </View>
+    <TouchableOpacity style={{
+      width: 40,
+      height: 40,
+      backgroundColor: COLORS.white,
+      position: 'absolute',
+      borderRadius: SIZES.extraLarge,
+      alignItems: 'center',
+      justifyContent: 'center',
+      ...SHADOWS.light,
+      ...props,
+    }} 
+    onPress={handlePress}
+    >
+      <Image  source={imgUrl}
+      resizeMode="contain"
+      style={{width: 24, height: 24}}/>
+      
+
+    </TouchableOpacity>
   )
 }
 export const RectButton = () => {
