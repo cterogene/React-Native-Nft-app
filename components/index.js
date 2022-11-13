bfbac886d8d5f1a6810459c2812fe8b0a328d299
Terogene Claudio <cterogene@gmail.com>
2022-11-13 10:41:48 -0500

    Added DetailsBid and DetailsDesc and update Detail main Component

--- components/index.js
+++ components/index.js
@@ -2,6 +2,9 @@ import HomeHeader from "./HomeHeader";
 import FocusedStatusBar from "./FocusedStatusBar";
 import NFTCard from "./NFTCard";
 import {CircleButton, RectButton} from "./Button";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
 export{
-    HomeHeader, FocusedStatusBar, NFTCard, CircleButton, RectButton
+    HomeHeader, FocusedStatusBar, NFTCard, CircleButton, RectButton,
+    DetailsBid, DetailsDesc
 } 
--- screens/Details.js
+++ screens/Details.js
@@ -1,8 +1,26 @@
-import React from 'react'
-import { Text } from 'react-native'
-const Details = () => {
+import { COLORS, SIZES, SHADOWS, assets } from '../constants';
+import { Text, View, SafeAreaView, Image, StatusBar, FlatList } from 'react-native'
+import { CircleButton, RectButton, SubInfo, FocusedStatusBar, DetailsDesc, DetailsBid } from '../components';
+
+const Details = (route, navigation) => {
   return (
-    <Text>Details</Text>
+    <SafeAreaView style={{
+      flex: 1
+    }}>
+      <FocusedStatusBar barStyle="dark-content"
+        backgroundColor="transparent"
+        transLucent={true} />
+
+      <View style={{
+        width: '100%',
+      }}>
+      </View>
+
+
+    </SafeAreaView>
+
+
+
   )
 }
 
